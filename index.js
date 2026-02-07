6ea77b3ce07982f657f400f1a557b6dfacaf9f32
Mayank7505703 <karansharma90900@gmail.com>
2026-02-07 18:06:30 +0530

    send api data to server that would ve visible on screen

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
-require('dotenv').config()
-const express= require("express")
-const app= express();
-app.get("/",(req,res)=>{
-  res.send("Server get a request on prot 3000")
-})
-app.get("/twitter",(req,res)=>{
-  res.send("Twitter is opened")
-  console.log("Request on twitter")
-})
-app.get("/login",(req,res)=>{
-  res.send(
-    '<h1>Please login at chai or code</h1>'
-  )
-})
-app.get("/youtube",(req,res)=>{
-  console.log("Youtube get a request")
-  res.send('<h2>youtube is opened</h2>')
-})
-const port=3000;
-app.listen(process.env.port,()=>{
-  console.log(`app is listening on port ${process.env.port}`)
-})
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+async function fetchData() {
+  try {
+    const response = await fetch(
+      "https://official-joke-api.appspot.com/random_joke",
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Fetch error:", error.message);
+  }
+}
+app.get("/data", async (req, res) => {
+  try {
+    const data = await fetchData();
+    res.json(data);
+  } catch (error) {
+    req.status(500).json({
+      error: "failed to fetch data",
+    });
+  }
+});
+app.listen(process.env.PORT, () => {
+  console.log(`App is listening on port ${process.env.PORT}`);
+});
